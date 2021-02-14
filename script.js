6535fe680fd5831226eec52856f51e1dfe0363b3
jensadria <jensadria@gmail.com>
2021-02-15 00:46:38 +0800

    Some styling changes

--- script.js
+++
@@ -1,6 +0,0 @@
-function showContent(event, contentID) {
-  const content = document.getElementById(contentID);
-
-  content.classList.toggle('active');
-  event.target.classList.toggle('active');
-}
